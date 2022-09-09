ba3e0b8ddcd1b58992d55166bad4cd6169e25420
DanielaOrbes <daniiorbes15@gmail.com>
2022-09-09 14:02:38 -0300

    vistas dinamicas ejs

---
+++ src/data/products.js
@@ -0,0 +1,26 @@
+module.exports=[
+    {
+        id: 1,
+        nombre: "Carpaccio fresco",
+        precio: 65.50,
+        descripcion: "El  carpaccio es un plato típico de la  cocina italiana. Nuestro plato esta elaborado a base de láminas muy finas de salmon , aderezadas con aceite de oliva, sal, cítricos y otros condimentos. Acompañado de lascas de queso parmesano. "
+    },
+    {
+        id: 2,
+        nombre: "Risoto de berenjena",
+        precio: 47.00,
+        descripcion: "Otro típico plato italiano. Risoto de berenjena de coccion lenta con arroz de la variedad carnaroli y queso de cabra. Se sugiere acompañar este delicioso platillo con una copa de nuestro vino del mes, Rutini sauvignon blanc. "
+    },
+    {
+        id: 3,
+        nombre: "Mousse de arroz",
+        precio: 27.50,
+        descripcion: "Mousse de arroz con leche y aroma de azahar"
+    },
+    {
+        id: 4,
+        nombre: "Espárragos blancos",
+        precio: 37.50,
+        descripcion: "Espárragos blancos con vinagreta de verduras y jamón ibérico"
+    }
+]
